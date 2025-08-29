892fe9c03cf6f544f72f0bb5d4e7f6b96853004f
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2025-08-29 20:01:10 +0530

    add pthread barrier code

---
+++ threads/14-pthread-barrier/main.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <unistd.h>
+
+#define NUM_THREADS 2
+
+// Thread barriers are used to synchronize threads
+// that are running in parallel
+// They stop the threads from executing until there are
+// a certain number of threads that have reached the barrier
+// and it allows all those threads to continue executing
+//
+// Eg:
+// Say the barrier is defined for 3 threads
+// If 2 threads have reached the barrier, they will have to wait
+// until the 3rd thread has reached the barrier
+
+pthread_barrier_t barrier;
+
+void *f()
+{
+    printf("Waiting at the barrier\n");
+
+    // Here, this barrier will wait until 3 threads have reached it
+    // If NUM_THREADS were 2, this barrier would never be broken
+    pthread_barrier_wait(&barrier);
+
+    printf("Reached the barrier\n");
+    return NULL;
+}
+
+int main()
+{
+    // Initialize the barrier with 3 threads
+    pthread_barrier_init(&barrier, NULL, 3);
+
+    // Create NUM_THREADS threads
+    pthread_t th[NUM_THREADS];
+    for (int i = 0; i < NUM_THREADS; i++)
+        pthread_create(th + i, NULL, &f, NULL);
+
+    // Join the threads
+    for (int i = 0; i < NUM_THREADS; i++)
+        pthread_join(th[i], NULL);
+
+    // Destroy the barrier
+    pthread_barrier_destroy(&barrier);
+    return 0;
+}
