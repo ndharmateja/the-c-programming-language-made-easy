d883a25ccbf72f765dadd1b25f8cead3c5f82a71
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2025-08-29 20:21:16 +0530

    add notes to double fork

--- processes/01-fork/double-fork.c
+++ processes/01-fork/double-fork.c
@@ -3,6 +3,10 @@
 
 int main()
 {
+    // The first fork will create two processes
+    // The each of the above 2 processes will run the second fork
+    // thereby creating a total of 4 processes
+    // all of which will print "Hello world"
     fork();
     fork();
     printf("Hello world!\n");
